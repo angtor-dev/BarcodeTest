59cae2897f1205530ae0dfe997f7adc18cf5e771
Angel Torres <angtor.dev@gmail.com>
2022-12-17 17:51:01 -0400

    Se elimino el uso de librerias para capturar snapshots

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using IronBarCode;
+using System.Drawing;
 
 namespace BarcodeTest.Controllers
 {
@@ -15,17 +16,19 @@ namespace BarcodeTest.Controllers
             _logger = logger;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
+            // Leyendo un codigo de barras desde una imagen
             BarcodeReaderOptions options = new BarcodeReaderOptions()
             {
                 Speed = ReadingSpeed.ExtremeDetail,
-                ExpectBarcodeTypes = BarcodeEncoding.UPCA | BarcodeEncoding.UPCE
+                ExpectBarcodeTypes = BarcodeEncoding.EAN13 | BarcodeEncoding.EAN8 | BarcodeEncoding.UPCA | BarcodeEncoding.UPCE | BarcodeEncoding.PharmaCode
             };
 
             timer.Restart();
             timer.Start();
-            BarcodeResults results = BarcodeReader.Read("Barcodes/barcode3.png", options);
+            BarcodeResults results = BarcodeReader.Read("Barcodes/barcode1.png", options);
             // BarcodeResult Result = BarcodeReader.ReadASingleBarcode("Barcodes/barcode2.png", BarcodeEncoding.AllOneDimensional, BarcodeReader.BarcodeRotationCorrection.Extreme, BarcodeReader.BarcodeImageCorrection.MediumCleanPixels);
 
             Console.WriteLine("Stop reading Barcode - " + timer.Elapsed.ToString());
@@ -36,12 +39,48 @@ namespace BarcodeTest.Controllers
                 Console.WriteLine("Value: " + result.Text + " Format: " + result.BarcodeType);
             }
 
-            //if (Result != null && Result.Text != "")
-            //{
-            //    Console.WriteLine("GetStarted was a success.  Read Value: " + Result.Text + " Format: " + Result.BarcodeType);
-            //}
-
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult Index(string image)
+        {
+            BarcodeResultViewModel model = new();
+
+            var imgBytes = Convert.FromBase64String(image);
+
+            timer.Restart();
+            timer.Start();
+
+            BarcodeResults Results = BarcodeReader.Read(imgBytes);
+
+            timer.Stop();
+
+            if (Results.Any())
+            {
+                BarcodeResult Result = Results.First();
+
+                Console.WriteLine("Codigo: " + Result.Text);
+                Console.WriteLine("Formato: " + Result.BarcodeType);
+
+                model.ResponseCode = 0;
+                model.Text = Result.Text;
+                model.Format = Result.BarcodeType.ToString();
+                model.TimeElapse = timer.Elapsed.ToString();
+
+                return Json(model);
+            }
+            else
+            {
+                Console.WriteLine("No se encontro nigun codigo de barras");
+
+                model.ResponseCode = 1;
+
+                return Json(model);
+            }
+
+            model.ResponseCode = 2;
+            return Json(model);
         }
 
         public IActionResult Privacy()
---
+++ wwwroot/js/test.js
@@ -0,0 +1,86 @@
+﻿const videoEl = document.getElementById("camara")
+
+navigator.mediaDevices.getUserMedia({
+    video: {
+        width: { min: 480, ideal: 1080 },
+        height: { min: 480, ideal: 1080 },
+        aspectRatio: { ideal: 1 },
+        facingMode: 'environment',
+    }
+})
+.then((stream) => {
+    localStream = stream
+    videoEl.srcObject = stream
+
+    // get the active track of the stream
+    const track = stream.getVideoTracks()[0]
+    console.log(track.getSettings())
+
+    // get current camera capabilities
+    let capabilities = track.getCapabilities()
+    console.log(capabilities)
+
+    // update a capability
+    // if (capabilities.brightness) {
+    //     track.applyConstraints({
+    //         advanced: [{
+    //             brightness: 0
+    //         }]
+    //     })
+    //     .catch(error => console.error("Error trying to apply constraints: ", error))
+    // }
+})
+
+function capture() {
+    var canvas = document.getElementById("canvas");
+    var video = document.getElementById("camara");
+    document.getElementById('codigo').textContent = "Decodificando..."
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas
+      .getContext("2d")
+      .drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+
+    image = canvas.toDataURL("image/png").split(';base64,')[1]
+
+    let formData = new FormData()
+    formData.append("image", image)
+
+    fetch('/Home/Index', {
+        method: 'post',
+        body: formData
+    })
+    .then(response => {
+        if (response.status == 200) {
+            return response.json()
+        }
+    })
+    .then(data => {
+        console.log(data)
+        if (data.responseCode == 0) {
+            document.getElementById('codigo').textContent = data.text
+            document.getElementById('formato').textContent = data.format
+            document.getElementById('tiempo').textContent = data.timeElapse
+        } else if (data.responseCode == 1) {
+            document.getElementById('codigo').textContent = "No se detecto ningun codigo de barras"
+            document.getElementById('formato').textContent = ""
+            document.getElementById('tiempo').textContent = ""
+        } else {
+            alert("Ocurrio un error inesperado")
+        }
+    })
+    .catch(error => {
+        console.error(error)
+    })
+  }
+
+  function stopCamera() {
+    try {
+        if (localStream) {
+            localStream.getTracks().forEach(track => track.stop());
+        }
+    } catch (e){
+        alert(e.message);
+    }
+}
